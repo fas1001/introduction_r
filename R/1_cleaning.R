86ede78cbd894067bcce281958aba0d8a8d2d651
LaurenceOMFoisy <mail@mfoisy.com>
2025-01-14 11:29:59 -0500

    finaliser exercice cleaning

--- R/1_cleaning.R
+++ R/1_cleaning.R
@@ -1,22 +1,30 @@
-# Nous voulons répondre à la question: Est-ce que les riches font plus de yoga?
+# Nous voulons répondre à la question:
+# « Est-ce que les riches font plus de yoga que les non-riche ?
 
-# 1. Importez la librairie dplyr qui contient les fonctions nécessaires pour manipuler les données dont select()
-
-
-# Chargez les données situées dans le chemin d'accès data/raw/data_raw.csv
-# Créez un dataframe nommé df et assignez les données à ce dataframe en utilisant la fonction read.csv()
+# 1. Importer la librairie dplyr qui contient les fonctions nécessaires pour
+# manipuler les données dont select()
 
 
-# Visualisez les données du dataframe en utilisant la fonction View()
+# 2. Charger les données situées dans le chemin d'accès data/raw/data_raw.csv
+# et Créez un dataframe nommé df et assignez les données à ce dataframe en
+# utilisant la fonction read.csv()
 
 
-# Générer la liste de tous les variables présentes dans notre dataframe en utilisant la fonction names()
+# 3. Visualiser les données du dataframe en utilisant la fonction View()
 
 
-# Utiliser la fonction select() pour sélectionner seulement les variables qui nous intéressent et les assigner à un nouveau dataframe nommé df_yoga
+# 4. Générer la liste de tous les variables présentes dans notre dataframe en
+# utilisant la fonction names()
 
 
-# On veut créer une variable dans lequel contient les valeurs riche, moyen et pauvre en utilisant la variable de revenu
+# 5. Utiliser la fonction select() pour sélectionner seulement les variables
+# qui nous intéressent et les assigner à un nouveau dataframe nommé df_yoga
 
 
-# On veut sauvegarder les données nettoyées dans un fichier csv. Utilisez la fonction write.csv() pour sauvegarder les données nettoyées dans le chemin d'accès data/clean/data_clean.csv
+# 6. Créer une variable dans lequel contient les valeurs
+# riche, moyen et pauvre en utilisant la variable de revenu
+
+
+# 7. Sauvegarder les données nettoyées dans un fichier csv. Utilisez la
+# fonction write.csv() pour sauvegarder les données nettoyées dans le chemin
+# d'accès data/clean/data_clean.csv
